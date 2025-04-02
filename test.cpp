526f9b48fe7651e9575d56c6b17db7dfddc448ab
Patrick Howland <minecraftbuilder2018@gmail.com>
2025-04-01 20:45:33 -0400

    Preferences interface creation; Fairly basic at the moment but functional

--- test.cpp
+++ test.cpp
@@ -9,78 +9,116 @@ public:
     virtual bool OnInit();
 };
 
-class TestFrame : public wxFrame {
-public:
-    TestFrame();
-
-private:
-    void OnHello(wxCommandEvent& event);
-    void OnExit(wxCommandEvent& event);
-    void OnAbout(wxCommandEvent& event);
-    void OnButton(wxCommandEvent& event);
+enum {
+    ID_vocabulary_set_lbl = 1,
+    ID_chapter_lbl = 2,
+    ID_game_lbl = 3,
+    ID_lbox_lbl = 4,
+    ID_vocabulary_set_combo = 5,
+    ID_chapter_combo = 6,
+    ID_game_combo = 7,
+    ID_finished_button = 8,
+    ID_vocabulary_types_lbox = 9,
 };
 
-enum {
-    ID_hello = 1,
-    ID_testbutton = 2,
-    ID_testcombo = 3
+class Preferences : public wxFrame {
+    // Labels
+    wxStaticText *vocabulary_set_lbl = new wxStaticText;
+    wxStaticText *chapter_lbl = new wxStaticText;
+    wxStaticText *game_lbl = new wxStaticText;
+    wxStaticText *lbox_lbl = new wxStaticText;
+
+    // Label points
+    wxPoint PT_vocabulary_set_lbl {0,0};
+    wxPoint PT_chapter_lbl {0,50};
+    wxPoint PT_game_lbl {0,100};
+    wxPoint PT_lbox_lbl {200,0};
+
+    // Label sizes
+    wxSize SZ_vocabulary_set_lbl {75, 25};
+    wxSize SZ_chapter_lbl {75, 25};
+    wxSize SZ_game_lbl {75, 25};
+    wxSize SZ_lbox_lbl {150, 25};
+
+    // Combo Boxes
+    wxComboBox *vocabulary_set_combo = new wxComboBox;
+    wxComboBox *chapter_combo = new wxComboBox;
+    wxComboBox *game_combo = new wxComboBox;
+
+    // Combo box points
+    wxPoint PT_vocabulary_set_combo {PT_vocabulary_set_lbl.x, PT_vocabulary_set_lbl.y + 25};
+    wxPoint PT_chapter_combo { PT_chapter_lbl.x, PT_chapter_lbl.y + 25 };
+    wxPoint PT_game_combo { PT_game_lbl.x, PT_game_lbl.y + 25 };
+
+    // Combo box sizes
+    wxSize SZ_vocabulary_set_combo {175, 25};
+    wxSize SZ_chapter_combo {175, 25};
+    wxSize SZ_game_combo {175, 25};
+
+    // Button
+    wxButton *finished_button = new wxButton;
+
+    // Button point
+    wxPoint PT_finished_button { PT_game_combo.x, PT_game_combo.y + 45};
+
+    // Button size
+    wxSize SZ_finished_button {175, 25};
+
+    // List box
+    wxListBox *vocabulary_types_lbox = new wxListBox;
+
+    // List box point
+    wxPoint PT_vocabulary_types_lbox { PT_lbox_lbl.x, PT_lbox_lbl.y + 25};
+
+    // List box size
+    wxSize SZ_vocabulary_types_lbox {150, 150};
+
+public:
+    Preferences()
+        : wxFrame(NULL, wxID_ANY, "Japanese Study Tool") {
+        // Labels
+        vocabulary_set_lbl -> Create(this, ID_vocabulary_set_lbl, "Vocabulary Set", PT_vocabulary_set_lbl, SZ_vocabulary_set_lbl);
+        chapter_lbl -> Create(this, ID_chapter_lbl, "Chapter", PT_chapter_lbl, SZ_chapter_lbl);
+        game_lbl -> Create(this, ID_game_lbl, "Game", PT_game_lbl, SZ_game_lbl);
+        lbox_lbl -> Create(this, ID_lbox_lbl, "Select Vocabulary Word Type", PT_lbox_lbl, SZ_lbox_lbl);
+
+        // Combo Boxes
+        vocabulary_set_combo -> Create(this, ID_vocabulary_set_combo, "Please select a vocabulary set", PT_vocabulary_set_combo, SZ_vocabulary_set_combo);
+        chapter_combo -> Create(this, ID_chapter_combo, "Please select a chapter", PT_chapter_combo, SZ_chapter_combo);
+        game_combo -> Create(this, ID_game_combo, "Please select a game", PT_game_combo, SZ_game_combo);
+
+        // Button
+        finished_button -> Create(this, ID_finished_button, "Finished!", PT_finished_button, SZ_finished_button);
+
+        // List box
+        vocabulary_types_lbox -> Create(this, ID_vocabulary_types_lbox, PT_vocabulary_types_lbox, SZ_vocabulary_types_lbox);
+
+        Bind(wxEVT_CHECKLISTBOX, &Preferences::set_chapters, this, ID_vocabulary_set_combo);
+        Bind(wxEVT_CHECKLISTBOX, &Preferences::set_vocabulary_types, this, ID_chapter_combo);
+        Bind(wxEVT_BUTTON, &Preferences::start_game, this, ID_finished_button);
+    }
+
+    void create_widgets() {
+    }
+
+private:
+    void set_chapters(wxCommandEvent& event) {
+        wxMessageBox("PLACEHOLDER", "Set chapters.");
+    }
+
+    void set_vocabulary_types(wxCommandEvent& event) {
+        wxMessageBox("PLACEHOLDER", "Set vocabulary types.");
+    }
+
+    void start_game(wxCommandEvent& event) {
+        wxMessageBox("PLACEHOLDER", "Start game.");
+    }
 };
 
 bool TestApp::OnInit() {
-    TestFrame *mainframe = new TestFrame();
+    Preferences *mainframe = new Preferences();
     mainframe -> Show(true);
     return true;
-}
-
-TestFrame::TestFrame()
-        : wxFrame(NULL, wxID_ANY, "Hello, World!") {
-        wxMenu *menuFile = new wxMenu;
-        menuFile -> Append(ID_hello, "&Hello...\tCtrl-H", "Help string shown in status bar for this menu item.");
-        menuFile -> AppendSeparator();
-        menuFile -> Append(wxID_ABOUT);
-
-        wxMenu *menuHelp = new wxMenu;
-        menuHelp -> Append(wxID_ABOUT);
-
-        wxMenuBar *menuBar = new wxMenuBar;
-        menuBar -> Append(menuFile, "&File");
-        menuBar -> Append(menuHelp, "&Help");
-
-        SetMenuBar(menuBar);
-
-        wxButton *Button = new wxButton;
-        wxPoint ButtonPoint {1, 51};
-        wxSize ButtonSize {100, 30};
-        Button -> Create(this, ID_testbutton, "BUTTON", ButtonPoint, ButtonSize);
-
-        wxComboBox *TestBox = new wxComboBox;
-        wxPoint ComboPoint {1,1};
-        wxSize ComboSize {250, 50};
-        TestBox -> Create(this, ID_testcombo, "COMBO BOX", ComboPoint, ComboSize);
-
-        CreateStatusBar();
-        SetStatusText("OMG hiiiiiiiiiiiiiiiiii");
-
-        Bind(wxEVT_MENU, &TestFrame::OnHello, this, ID_hello);
-        Bind(wxEVT_MENU, &TestFrame::OnAbout, this, wxID_ABOUT);
-        Bind(wxEVT_MENU, &TestFrame::OnExit, this, wxID_EXIT);
-        Bind(wxEVT_BUTTON, &TestFrame::OnButton, this, ID_testbutton);
-}
-
-void TestFrame::OnExit(wxCommandEvent& event) {
-    Close(true);
-}
-
-void TestFrame::OnAbout(wxCommandEvent& event) {
-    wxMessageBox("This is a wxWidgets Hello World example", "About Hello World", wxOK | wxICON_INFORMATION);
-}
-
-void TestFrame::OnHello(wxCommandEvent& event) {
-    wxLogMessage("Hello world from wxWidgets!");
-}
-
-void TestFrame::OnButton(wxCommandEvent& event) {
-    wxMessageBox("HIIII HI HIIIIIIIIIIII", "Teehee :3");
 }
 
 wxIMPLEMENT_APP(TestApp);
