ce37344def35f975dc9777cd618bb7acfff9b416
patrick <minecraftbuilder2018@gmail.com>
2025-08-18 14:57:16 -0400

    LAST ONE I SWEAR

--- .ycm_extra_conf.py
+++
@@ -1,5 +0,0 @@
-def Settings( **kwargs ):
-  return {
-    'flags': ['-x', 'c++', '-Wall', '-Wextra', '-Werror', '-I/usr/lib/wx/include/gtk3-unicode-3.2', '-I/usr/include/wx-3.2', '-DWXUSINGDLL',
-              '-D__WXGTK3__', '-D__WXGTK__', '-D_FILE_OFFSET_BITS=64', '-g', '-I~/Japanese-Study-Tool',],
-  }
